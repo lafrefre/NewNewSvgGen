7a6916e860ed5fd772d4940eb86e6ba0b3af674c
lafrefre <134004859+lafrefre@users.noreply.github.com>
2023-11-09 20:00:21 -0700

    "made my test and got cl to generate shapes"

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 const inquirer = require('inquirer');
 const {Triangle, Square, Circle} = require('./lib/shapes');
 const genSVG = require('./lib/genSVG');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters:',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['Triangle', 'Square', 'Circle'],
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a text color:',
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a shape color:',
+    },
+];
+
+inquirer.prompt(questions).then((answers) => {
+    const {text, shape, textColor, shapeColor} = answers;
+    
+    const shapeInstance = new (eval(shape))();
+    shapeInstance.setColor(shapeColor);
+
+    genSVG(shapeInstance, textColor, shapeColor, text);
+});
